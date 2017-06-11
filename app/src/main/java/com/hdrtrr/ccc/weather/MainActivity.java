3be0edc0ac3da99bef571ab6ee84a9386fb7522b
hdrtrr <gzrjjmh@qq.com>
2017-06-11 21:30:14 +0800

    First commit.

---
+++ app/src/main/java/com/hdrtrr/ccc/weather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hdrtrr.ccc.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
