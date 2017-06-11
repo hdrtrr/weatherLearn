9ea30907dedb291da1ca386a4a670ac071c267cb
hdrtrr <gzrjjmh@qq.com>
2017-06-11 22:10:09 +0800

    测试11

--- app/src/main/java/com/hdrtrr/ccc/weather/MainActivity.java
+++ app/src/main/java/com/hdrtrr/ccc/weather/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        首次使用git代码托管
+        int test = 12;
+        int ss = 22;
     }
 }
